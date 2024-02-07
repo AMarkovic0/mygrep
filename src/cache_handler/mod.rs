e4dbc4045498ea517447e0aac9e01be8829ab6d1
AMarkovic0 <malexa96@gmail.com>
2024-02-07 23:23:26 +0100

    Create config dir if it does not exist

--- src/cache_handler/mod.rs
+++ src/cache_handler/mod.rs
@@ -1,5 +1,6 @@
 mod config_file;
 
+use std::fs;
 use std::path::PathBuf;
 use crate::cache_handler::config_file::ConfigFile;
 
@@ -12,6 +13,9 @@ impl Cache {
     pub fn new(mut home_dir: PathBuf) -> Option<Cache> {
         if home_dir.exists() {
             home_dir.push(".pgrep");
+
+            let _ = Cache::create_config_dir(&home_dir)
+                .expect("ERROR: Failed to create config dir");
 
             return Some(Cache {
                 config: ConfigFile::new(
@@ -26,6 +30,22 @@ impl Cache {
         }
 
         None
+    }
+
+    fn create_config_dir(home_dir: &PathBuf) -> std::io::Result<()>{
+        if !home_dir.exists() {
+            if let Some(dir_path) = home_dir.to_str() {
+                let _ = fs::create_dir(dir_path);
+            } else {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Config directory path str not found"
+                ));
+            }
+
+        }
+
+        return Ok(());
     }
 
     pub fn cache_history(&self, args: &String) -> std::io::Result<()> {
